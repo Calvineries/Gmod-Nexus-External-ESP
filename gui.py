4344c8ef2d7246f7b134a99982c9cceaeedbbb05
Calvineries <Calvineries@gmail.com>
2025-09-24 17:13:49 +0200

    force build

--- gui.py
+++ gui.py
@@ -177,7 +177,7 @@ class GUI():
 
                 with dpg.tab(label='About'):
                     dpg.add_text("[Nexus External ESP]")
-                    dpg.add_text("Version: 2.2.2b")
+                    dpg.add_text("Version: 2.2.2c")
                     if "offline" in {app.Offsets.lastupdate}:
                         dpg.add_text(f"Custom offsets mode.")
                     else:
@@ -200,4 +200,5 @@ class GUI():
         dpg.setup_dearpygui()
         dpg.show_viewport()
         dpg.set_primary_window("w_main", True)
+
 
